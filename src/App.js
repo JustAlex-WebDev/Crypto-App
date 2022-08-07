d4b5666ac084a430afc2089384b56f1758ff9567
Alexandar Valov <alexvulov123@gmail.com>
2022-08-07 11:12:03 +0300

    Created and the Navbar Component and the Theme Toggle button (Component)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-  return <ThemeProvider></ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
