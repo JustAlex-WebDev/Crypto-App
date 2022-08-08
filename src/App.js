082030f3a4b0f1aedd8fbf55ea63b96b7a48916d
Alexandar Valov <alexvulov123@gmail.com>
2022-08-08 10:21:43 +0300

    Added almost fully functioning Coin Search and fetched the coins from the CoinGecko API using axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import Signin from "./routes/Signin";
+import Signup from "./routes/Signup";
+import Account from "./routes/Account";
+import axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      // console.log(response.data);
+    });
+  }, [url]);
+
   return (
     <ThemeProvider>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home coins={coins} />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </ThemeProvider>
   );
 }
---
+++ src/components/CoinSearch.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+
+const CoinSearch = ({ coins }) => {
+  return (
+    <div>
+      <div>
+        <h1>Search Crypto</h1>
+        <form>
+          <input type="text" placeholder="Search a coin" />
+        </form>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>24h Volume</th>
+            <th>Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <AiOutlineStar />
+              </td>
+              <td>{coin.market_cap_rank}</td>
+              <td>
+                <div>
+                  <img src={coin.image} alt={coin.id} />
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+              <td>{coin.symbol}</td>
+              <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h}</td>
+              <td>{coin.total_volume}</td>
+              <td>{coin.market_cap}</td>
+              <td>{coin.sparkline_in_7d.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CoinSearch;
