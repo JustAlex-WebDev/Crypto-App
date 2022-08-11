71aa8c332e5dddfaedea4ae3c6dc7b32f3ee132a
Alexandar Valov <alexvulov123@gmail.com>
2022-08-11 09:20:44 +0300

    Added and fully styled the Footer Component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Signup from "./routes/Signup";
 import Account from "./routes/Account";
 import CoinPage from "./routes/CoinPage";
 import axios from "axios";
+import Footer from "./components/Footer";
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -34,6 +35,7 @@ function App() {
           <Route path=":coinId" />
         </Route>
       </Routes>
+      <Footer />
     </ThemeProvider>
   );
 }
